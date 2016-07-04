818d77f4a9e95f5bacb5404accbba91c2e8aa200
Shubham <=agshubh191@gmail.com>
2016-07-04 23:25:00 +0530

    Added best matching url
    Added :
    - Removed extension from fileName
    - Used best matching name

--- extracter.py
+++ extracter.py
@@ -4,8 +4,13 @@ from StringIO import StringIO
 from zipfile import ZipFile
 import requests 
 import sys
+import os
+from fuzzywuzzy import process
+from pprint import pprint
 
+# Get Movie Name
 movie = sys.argv[1]
+movie = os.path.splitext(movie)[0]
 
 base_url = 'https://subscene.com/'
 response = requests.get(base_url+'subtitles/release?q='+movie)
@@ -14,24 +19,28 @@ myTable = soup.table
 rows = myTable.tbody.findAll('tr')
 # Take Language as input or global parameter set by user
 LANGUAGE = 'english'
-possible_urls = []  
+possible_urls = {}  
 
 for row in rows : 
     lan = row.find('span', {'class':'l r neutral-icon'})
     if lan == None : 
-        print 'lan None : ', row
         continue
     if lan.stripped_strings.next().lower() == LANGUAGE :
-        movie = row.findAll('span')[1].string.rstrip().lstrip()
+        name = row.findAll('span')[1].string.rstrip().lstrip()
         sub_url = row.a['href'] 
-        possible_urls.append((movie, sub_url))
+        possible_urls[name] =  sub_url
 
-#TODO : Find best possible matching string
-subtitle_url = possible_urls[0][1]
+print 'List of available options : '
+pprint([url for url in possible_urls])
+url = process.extractOne(movie, [url for url in possible_urls])[0]
+subtitle_url = possible_urls[url]
 response = requests.get(base_url + subtitle_url)
 soup = BeautifulSoup(response.content)
 mydiv = soup.find('div', {'class':'download'})
 download_url = base_url + mydiv.a['href']
 response = requests.get(download_url)
+# Extract Zip File 
 subZipFile = StringIO(response.content)
 ZipFile(subZipFile).extractall()
+print 'Subtitle file : ', url, 'downloaded and extracted'  
+#TODO : Download all in a directory
